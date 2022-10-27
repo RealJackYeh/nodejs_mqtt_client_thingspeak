244e7d742a556650753531e88fbb3636cf7f2ec3
Jack Yeh <realjackyeh@gmail.com>
2022-10-27 20:21:15 +0800

    v0.2

--- nodejs_mqtt_client_thingspeak.js
+++ nodejs_mqtt_client_thingspeak.js
@@ -1,9 +1,9 @@
 const mqtt = require('mqtt');
 let url = 'mqtt://mqtt3.thingspeak.com' 
 let options = {
-    clientId:"Fj03BQYSBx8ABgw2GCAkPC4",
-    username:"Fj03BQYSBx8ABgw2GCAkPC4",
-    password:"OdH6j3y3ZfZwHfSPZO6SyGkd",
+    clientId:"your own clientId",
+    username:"your own username",
+    password:"your own password",
     clean:true
 }
 // 連接 MQTT server
@@ -13,13 +13,13 @@ client.on("error",function(error){
     process.exit(1)
 })
 // 訂閱訊息
-let topic_list = ["channels/1898172/subscribe/fields/field2","channels/1898172/subscribe/fields/field3"] 
+let topic_list = ["channels/<your own channel number>/subscribe/fields/field2","channels/1898172/subscribe/fields/field3"] 
 client.subscribe(topic_list, {qos:0})
 client.on('message', function(topic, message){
 	console.log(topic, ": "+ message.toString())
 })
 // 發佈訊息
-let publish_topic = "channels/1898172/publish/fields/field1"
+let publish_topic = "channels/<your own channel number>/publish/fields/field1"
 let i = 0
 setInterval(function(){
     if (i == 0) i = 1
